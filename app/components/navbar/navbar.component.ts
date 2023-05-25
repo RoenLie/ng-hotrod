2e54748ae2b8a2e0ef5ec22a830ceaab5ec5b567
KristofferRoenLie <kristoffer.roenlie@eye-share.no>
2023-05-25 13:05:28 +0200

    css module test.

--- app/components/navbar/navbar.component.ts
+++ app/components/navbar/navbar.component.ts
@@ -1,20 +1,27 @@
-import './navbar.styles.scss';
-
 import controller from './navbar.controller.js';
+import styles from './navbar.styles.module.scss';
+
 
 const navbarComponent = {
 	bindings: {},
-	template: `
-		<md-toolbar class="navbar">
+	template: /* html */`
+		<md-toolbar>
 			<div class="md-toolbar-tools">
 				<div flex layout="row" layout-align="start center">
-					<img class="ng-hotrod-logo" ng-src="{{ $ctrl.ngHotrodLogo }}" alt="GitHub">
-					<h2 class="active-page-name" md-truncate>{{ $ctrl.NavigationService.currentPageTitle }}</h2>
+					<img class="${ styles['ng-hotrod-logo'] }"
+						ng-src="{{ $ctrl.ngHotrodLogo }}"
+						alt="GitHub">
+					<h2 class="${ styles['active-page-name'] }"
+						md-truncate>{{ $ctrl.NavigationService.currentPageTitle }}
+					</h2>
 				</div>
+
 				<a
-					class="github-link" href="https://github.com/fyodorio/ng-hotrod"
+					class="${ styles['github-link'] }"
+					href="https://github.com/fyodorio/ng-hotrod"
 					layout="row"
-					layout-align="center center">
+					layout-align="center center"
+				>
 					<img ng-src="{{ $ctrl.githubLogo }}" alt="GitHub">
 					<div>GitHub</div>
 				</a>
@@ -23,5 +30,8 @@ const navbarComponent = {
 	`,
 	controller,
 };
+
+console.log(navbarComponent);
+
 
 export default navbarComponent;
